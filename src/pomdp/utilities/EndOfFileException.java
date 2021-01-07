6fca09e16c0bc3af3e3b6e8eef7ad7cdabddac83
keets zhu <928420597@qq.com>
2021-01-07 23:56:07 +0800

    init

---
+++ src/pomdp/utilities/EndOfFileException.java
@@ -0,0 +1,26 @@
+/*
+ * Created on May 3, 2005
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+
+/**
+ * @author shanigu
+ * <p>
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package pomdp.utilities;
+
+import java.io.IOException;
+
+public class EndOfFileException extends IOException {
+    public EndOfFileException() {
+        super();
+    }
+
+    public EndOfFileException(String sData) {
+        super(sData);
+    }
+}
